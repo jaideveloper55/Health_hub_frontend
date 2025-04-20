3b6593305bea754d319f2add40ee0e6724c658d6
jaiganesh <jaiganesh.thirthapathi@ticvic.com>
2025-04-20 08:19:32 +0530

    updated-folder-path

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import Doctors from "./pages/Doctors";
 import Navbar from "./components/Navbar";
 import MyProfile from "./pages/MyProfile";
